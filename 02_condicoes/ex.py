9414afd878232f9324e7dc07fd9772f9f9329655
GoncaloaaF <goncaloaaf@gmail.com>
2025-09-30 11:53:06 +0100

    fix typo

--- 02_condicoes/ex.py
+++ 02_condicoes/ex.py
@@ -3,7 +3,7 @@ faça um porgrama onde é pedido ao utlizador um ano,
  com base no ano deve calcular a idade (pode assumir que o ano atual e 2025)
  - se tiver mais de 18 anos deve indicar adulto,
  - se tiver entre 12 e 18 deve indicar adolescente
- - se tiver menos de 18 deve indicar criança
+ - se tiver menos de 12 deve indicar criança
 
-deve usar o if, elif else 
+deve usar o if, elif else
 """

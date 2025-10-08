e0a550ee7cb2ff545592338bb4ad9337d1bdac70
GoncaloaaF <goncaloaaf@gmail.com>
2025-10-08 15:02:35 +0100

    demo funcs -> def

---
+++ 05_funcs/main.py
@@ -0,0 +1,38 @@
+"""
+if - conduções
+for - loops
+etc etc
+
+def - funções
+
+
+"""
+
+
+def ola_mundo():# c -> void ola_mundo(){printf("Ola Mundo2")}
+    print("Ola Mundo")
+
+
+print("Linha 1")
+
+ola_mundo()
+ola_mundo()
+ola_mundo()
+print("Linha 2")
+
+
+
+def ola_mundo2(nome):
+    print(f"Ola Mundo, {nome}")
+
+
+ola_mundo2("Rita")
+ola_mundo2("Carlos")
+ola_mundo2("Pedro")
+
+
+"""
+
+Crie uma função que receber um numero e diga se ele e par ou impar 
+
+"""
